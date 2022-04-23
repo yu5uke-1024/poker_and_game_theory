e208931f9f5dfaf239625388f0ba99ca1812f5a8
Yusuke <yusuke1024@gmail.com>
2022-04-23 17:27:14 +0900

    a

---
+++ FP/FSP_Kuhn_Poker/FSP_Kuhn_Poker.py
@@ -0,0 +1,17 @@
+#ライブラリ
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import itertools
+from collections import defaultdict
+import sys
+from tqdm import tqdm
+import time
+import doctest
+import copy
+from sklearn.neural_network import MLPClassifier
+from collections import deque
+
+import warnings
+warnings.filterwarnings('ignore')
---
+++ その他/debug.py
@@ -0,0 +1,8 @@
+
+import random
+import numpy as np
+import math
+
+a = math.prod(np.array([1,2,3]))
+
+print(a)
